073f6b18dc954cd6b67102406331aee8bea92315
Zoro-OP <shoaibhossain207@gmail.com>
2020-11-21 05:50:00 +0600

    1st modification Admin by Shoieb

---
+++ html/Admin_login.php
@@ -0,0 +1,32 @@
+
+<html>
+	<head>
+		<title>Admin Login</title>
+  <link rel="stylesheet" href="../css/registration.css"></head>
+	<body>
+
+		<fieldset id="register_form" align= "center">
+      <h1>Login</h1>
+
+			<form action="" method="post"  >
+				<table>
+         
+          <tr>
+          <td style="text-align: left;"> Username:</td>
+          <td style="text-align: left;"><input type="text" name="username">
+          </td>
+
+          <tr>
+          <td style="text-align: left;"> Password:</td>
+          <td style="text-align: left;"><input type="password" name="password">
+          </tr>
+          
+          <td colspan="2" align="center">
+          <input type="submit" name="register" value="Login">
+          </td>
+          </tr>
+				</table>
+			</form>
+		</fieldset>
+	</body>
+</html>
