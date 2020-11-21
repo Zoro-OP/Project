0f01be9f24dd0324333c59cb3c51f6de34dc8907
Zoro-OP <shoaibhossain207@gmail.com>
2020-11-21 06:57:03 +0600

    Candidates Checking by Admin

---
+++ html/Admin_SeeAppliedCandidates.php
@@ -0,0 +1,102 @@
+	<head>
+		<title>Applied Candidates Info</title>
+  <link rel="stylesheet" href="../css/registration.css"></head>
+	<body>
+
+		<fieldset id="register_form" align= "center">
+      <h1>Applied Candidates Info</h1>
+
+	  
+<form action="" method="post">
+<table>
+
+<tr>
+<td>
+<h2>Applied Candidates list</h2>
+<tr>
+<td>
+
+<table border ="1" style="width:400px">
+  <tr>
+  <th>Candidates Id</th>
+  <th>Candidates Name</th>
+  <th>Candidates Image</th>
+	<th>CV</th>
+	<th>Job Experience</th>
+	<th>Job Type</th>
+	<th>Expected Salary</th>
+  </tr>
+  
+  <tr>
+  <td>01</td>
+  <td>Md. Shoieb Hossain</td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="Image" value="Image"></td>
+	<td colspan="1.5" align="middle">
+  <input type="submit" name="File" value="File"></td>
+	<td>1 year</td>
+	<td>Full-time</td>
+  <td>20000Tk</td>
+	</tr>
+  
+  <tr>
+  <td>02</td>
+  <td>Md. Manik</td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="Image" value="Image"></td>
+	<td colspan="1.5" align="middle">
+  <input type="submit" name="File" value="File"></td>
+	<td>5 years</td>
+  <td>Full-time</td>
+  <td>60000Tk</td>
+  </tr>
+
+  <tr>
+  <td>03</td>
+  <td>Ms. Neha</td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="Image" value="Image"></td>
+	<td colspan="1.5" align="middle">
+  <input type="submit" name="File" value="File"></td>
+	<td>Null</td>
+  <td>Part-time</td> 
+  <td>15000Tk</td>
+  </tr>
+
+  <tr>
+  <td>04</td>
+  <td>Ms. Anonna</td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="Image" value="Image"></td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="File" value="File"></td>
+  <td>2 years</td>
+  <td>Full-time</td> 
+  <td>25000Tk</td>
+  </tr>
+
+  <tr>
+  <td>05</td>
+  <td>Md. Nawlen</td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="Image" value="Image"></td>
+  <td colspan="1.5" align="middle">
+  <input type="submit" name="File" value="File"></td>
+  <td>Null</td>
+  <td>Part-time</td>
+  <td>12000Tk</td>
+  </tr>
+
+  
+</table>
+</td>
+</tr>
+
+<tr></tr><tr><tr></tr>
+
+</tr>
+
+      </form>
+		</fieldset>
+	</body>
+</html>
